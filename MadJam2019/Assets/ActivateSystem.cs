ee38b25631e62eb76cd488d42b1244050858997b
Chuck <washinima99@gmail.com>
2019-02-23 04:15:48 +0000

    idk everything

---
+++ MadJam2019/Assets/ActivateSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private ParticleSystem ps;
+    public int nrPArticles;
+
+
+    void Start()
+    {
+        this.ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ps.Emit(nrPArticles);
+        }
+
+    }
+}
---
+++ MadJam2019/Assets/ScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Object : ScriptableObject
+{
+    // For the Thunder Object
+    public ParticleSystem particleSystem;
+
+    public List<Sprite> spriteList;
+
+    public float Velocity;
+
+    public float LifeTime;
+}
