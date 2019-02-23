36b212d628cd36ce10ea7f53e992ad1216354aec
Chuck <washinima99@gmail.com>
2019-02-23 16:37:27 +0000

    Let's see

--- MadJam2/Assets/Scripts/ScriptableObject.cs
+++ MadJam2/Assets/Scripts/ScriptableObject.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
-public class Object : ScriptableObject
+public class HazardObjects : ScriptableObject
 {
     // For the Thunder Object
     public ParticleSystem particleSystem;
 
     public List<Sprite> spriteList;
 
-    public float Velocity;
+    public float Mass;
 
     public float LifeTime;
 }
